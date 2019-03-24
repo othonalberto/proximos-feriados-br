a17ee7fce182c21ebf0fa06c37cf504f26d04edf
othonalberto <othonalberto@gmail.com>
2019-03-23 23:35:11 -0300

    resolves #1

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 const fetch = require('node-fetch')
 
+async function consomeAPI (ano, n = 1) {
+  // Esta função retorna um vetor de duas posições
+  // [0]: feriados do ano atual
+  // [1]: feriados do ano seguinte, caso N seja maior que a quantidade de feriados deste ano
+  // Em caso de erro ao consumir a API, retorna Error('Erro ao consumir API de feriados.')
+
+  const anoAtual = parseInt(ano)
+
+  try {
+    const resposta = await fetch('https://date.nager.at/api/v2/publiccholidays/' + anoAtual + '/BR')
+    const dados = await resposta.json()
+
+    const proximoAno = (n > dados.length) ? (await fetch('https://date.nager.at/api/v2/publicholidays/' + (anoAtual + 1) + '/BR')) : null
+    const dadosProx = (proximoAno) ? await proximoAno.json() : null
+
+    return [dados, dadosProx]
+  } catch (e) {
+    throw new Error('Erro ao consumir API de feriados.')
+  }
+}
+
 async function obtemFeriados (dataHoje, n = 1) {
   const dataSeparada = dataHoje.toString().split('/')
   const mes = dataSeparada[1]
   const dia = dataSeparada[2]
   const ano = dataSeparada[0]
 
-  const resposta = await fetch('https://date.nager.at/api/v2/publicholidays/' + ano + '/BR')
-  const dados = await resposta.json()
+  try {
+    const retornoAPI = await consomeAPI(ano, n)
 
-  const proximoAno = (n > dados.length) ? (await fetch('https://date.nager.at/api/v2/publicholidays/' + (ano + 1) + '/BR')) : null
-  const dadosProx = (proximoAno) ? await proximoAno.json() : null
+    const proximosFeriadosAno = retornoAPI[0].filter(x => {
+      return (parseInt(x.date.split('-')[1]) > mes ||
+             (parseInt(x.date.split('-')[1]) === mes && parseInt(x.date.split('-')[2]) > dia)
+      )
+    })
 
+    const todos = (retornoAPI[1]) ? proximosFeriadosAno.concat(retornoAPI[0]) : proximosFeriadosAno
 
-  const proximosFeriadosAno = dados.filter(x => {
-    return (parseInt(x.date.split('-')[1]) > mes ||
-           (parseInt(x.date.split('-')[1]) === mes && parseInt(x.date.split('-')[2]) > dia)
-    )
-  })
+    const resultado = todos.slice(0, n).map(x => {
+      return {
+        'nome': x.localName,
+        'data': formataDataBR(x.date)
+      }
+    })
 
-  const todos = (dadosProx) ? proximosFeriadosAno.concat(dadosProx) : proximosFeriadosAno
-
-  const resultado = todos.slice(0, n).map(x => {
-    return {
-      'nome': x.localName,
-      'data': formataDataBR(x.date)
-    }
-  })
-
-  return resultado
+    return resultado
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 function formataDataBR (data) {
