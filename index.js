8d5e413c299246dd78ab6e0f5544221a7c8fba0b
othonalberto <othonalberto@gmail.com>
2020-04-21 13:52:58 -0300

    Melhorias na construcao

--- index.js
+++ index.js
@@ -1,67 +1,78 @@
-const fetch = require('node-fetch')
+import fetch from 'node-fetch';
 
-async function consomeAPI (ano, n = 1) {
-  // Esta função retorna um vetor de duas posições
-  // [0]: feriados do ano atual
-  // [1]: feriados do ano seguinte, caso N seja maior que a quantidade de feriados deste ano
-  // Em caso de erro ao consumir a API, retorna Error('Erro ao consumir API de feriados.')
+async function consomeAPI (ano) {
+    try {
+        const requestUrl = constroiRequest(parseInt(ano));
 
-  const anoAtual = parseInt(ano)
+        const resposta = await fetch(requestUrl);
 
-  try {
-    const resposta = await fetch('https://date.nager.at/api/v2/publiccholidays/' + anoAtual + '/BR')
-    const dados = await resposta.json()
-
-    const proximoAno = (n > dados.length) ? (await fetch('https://date.nager.at/api/v2/publicholidays/' + (anoAtual + 1) + '/BR')) : null
-    const dadosProx = (proximoAno) ? await proximoAno.json() : null
-
-    return [dados, dadosProx]
-  } catch (e) {
-    throw new Error('Erro ao consumir API de feriados.')
-  }
+        return resposta.json();
+    } catch (e) {
+        throw new Error(`Erro ao consumir API de feriados: ${e}`);
+    }
 }
 
 async function obtemFeriados (dataHoje, n = 1) {
-  const dataSeparada = dataHoje.toString().split('/')
-  const mes = dataSeparada[1]
-  const dia = dataSeparada[2]
-  const ano = dataSeparada[0]
+    const dataSeparada = dataHoje.toString().split('/');
+    const ano = parseInt(dataSeparada[0]);
 
-  try {
-    const retornoAPI = await consomeAPI(ano, n)
+    try {
+        const retornoAPI = await consomeAPI(ano, n);
 
-    const proximosFeriadosAno = retornoAPI[0].filter(x => {
-      return (parseInt(x.date.split('-')[1]) > mes ||
-             (parseInt(x.date.split('-')[1]) === mes && parseInt(x.date.split('-')[2]) > dia)
-      )
-    })
+        const feriadosAnoAtual = retornoAPI.filter((feriado) => feriadoValido(feriado, dataSeparada));
 
-    const todos = (retornoAPI[1]) ? proximosFeriadosAno.concat(retornoAPI[0]) : proximosFeriadosAno
+        const feriadosProximoAno = (feriadosAnoAtual.length < n) ? await consomeAPI(ano + 1) : null;
 
-    const resultado = todos.slice(0, n).map(x => {
-      return {
-        'nome': x.localName,
-        'data': formataDataBR(x.date)
-      }
-    })
+        const feriados = feriadosAnoAtual.concat(feriadosProximoAno);
 
-    return resultado
-  } catch (e) {
-    console.error(e)
-  }
+        const resultado = feriados.slice(0, n).map((feriado) => constroiObjetoFeriado(feriado));
+
+        return resultado;
+    } catch (e) {
+        console.error(e);
+        return e;
+    }
 }
 
-function formataDataBR (data) {
-  const separados = data.toString().split('-')
+function formataDataBR(data) {
+    const separados = data.toString().split('-');
 
-  return (separados[2] + '/' + separados[1] + '/' + separados[0])
+    return (`${separados[2]}/${separados[1]}/${separados[0]}`);
 }
 
-module.exports.proximosFeriados = function (n = 1) {
-  const num = (parseInt(n) > 0) ? (parseInt(n)) : 1
+function constroiObjetoFeriado(data) {
+    const obj = {
+        nome: data.localName,
+        data: formataDataBR(data.date),
+    };
 
-  const d = new Date()
-  const dataCompleta = d.getFullYear() + '/' + (d.getUTCMonth() + 1) + '/' + d.getDate()
+    return obj;
+}
 
-  return obtemFeriados(dataCompleta, num)
+function feriadoValido(feriado, dataAtual) {
+    const dataSeparada = feriado.date.split('-');
+    const mes = parseInt(dataSeparada[1]);
+    const dia = parseInt(dataSeparada[2]);
+
+    const mesAtual = parseInt(dataAtual[1]);
+    const diaAtual = parseInt(dataAtual[2]);
+
+    if (mes === mesAtual) {
+        return (dia > diaAtual);
+    }
+
+    return (mes > mesAtual);
+}
+
+function constroiRequest(ano) {
+    return `https://date.nager.at/api/v2/PublicHolidays/${ano}/BR`;
+}
+
+export function proximosFeriados (n = 1) {
+    const num = (parseInt(n) > 0) ? (parseInt(n)) : 1;
+
+    const d = new Date();
+    const dataCompleta = `${d.getFullYear()}/${d.getUTCMonth() + 1}/${d.getDate()}`;
+
+    return obtemFeriados(dataCompleta, num);
 }
